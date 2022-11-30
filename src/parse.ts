277c8b39fb50ba66b1d2f5ed1e616931b93a5bad
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2022-12-01 00:41:46 +0100

    feat: Improvements

--- src/parse.ts
+++ src/parse.ts
@@ -1,10 +1,13 @@
-const ora = require("ora");
-const path = require("path");
-const chalk = require("chalk");
-const json2csv = require("json2csv");
+// const ora = require("ora");
+// const path = require("path");
+// const chalk = require("chalk");
+// const json2csv = require("json2csv");
+
 const fs = require("fs-extra");
 const readDir = require("readdir");
-const format = require("date-fns/format");
+
+import { format } from "date-fns";
+import csv from "csvtojson";
 
 const INPUT_CSV_FILEPATH = "./csv/input/";
 const SANITIZED_CSV_FILEPATH = "./csv/sanitized/";
@@ -26,20 +29,30 @@ function getCSVFilePaths({
   });
 }
 
-async function sanitizeCSVFile({
-  filePath,
-  index,
-}: {
-  filePath: string;
-  index: number;
-}) {
+async function sanitizeCSVFile(filePath: string) {
   let targetFileName = filePath;
+
+  /**
+   * Cleanup all nordic characters
+   */
+  const fileContent = await fs.readFile(
+    `${INPUT_CSV_FILEPATH}${filePath}`,
+    "utf8"
+  );
+
+  const sanitisedFileContent = fileContent.replaceAll(/�/g, "oe");
+
+  await fs.writeFile(
+    `${INPUT_CSV_FILEPATH}${filePath}`,
+    sanitisedFileContent,
+    "utf8"
+  );
 
   // Only format non-formatted files
   if (!filePath.includes("sydbank_csv_")) {
-    targetFileName = `sydbank_csv_${index}_${format(
+    targetFileName = `sydbank_csv_${format(
       new Date(),
-      "DD-MM-YYYY_hh.mm.ss"
+      "dd-MM-yyyy_hh.mm.ss"
     )}.csv`;
   }
 
@@ -49,36 +62,118 @@ async function sanitizeCSVFile({
   );
 }
 
-async function parseCSVFile(filePath: string, index: number) {
-  const fullPath = `${OUTPUT_CSV_FILEPATH}${filePath}`;
-  console.log(">>>>>> filePath: ", { filePath, index });
-  console.log(">>>>>> fullPath: ", fullPath);
+interface ParsedEntry {
+  Date: string;
+  Payee: string;
+  Category: string;
+  Memo: string;
+  Outflow: string;
+  Inflow: string;
+}
+
+const YNAB_FIELDS = ["Date", "Payee", "Category", "Memo", "Outflow", "Inflow"];
+
+async function parseCSVFile(filePath: string) {
+  const fullPath = `${SANITIZED_CSV_FILEPATH}${filePath}`;
+
+  function parseCSV(jsonObject) {
+    const output = [];
+
+    jsonObject.forEach((entryObject) => {
+      const parsedEntry: ParsedEntry = {
+        Date: "",
+        Payee: "",
+        Category: "",
+        Memo: "",
+        Outflow: "",
+        Inflow: "",
+      };
+
+      /**
+       * Parse date
+       */
+      const entryDate = entryObject["Dato"];
+      if (entryDate) {
+        parsedEntry.Date = entryDate.replaceAll(".", "/");
+      }
+
+      /**
+       * Parse inflow/outflow
+       */
+      const entryAmount = entryObject["Bel�b"] ?? entryObject["Beloeb"];
+      if (entryAmount) {
+        const parsedAmount =
+          Number(entryAmount.replace(/[^0-9\-]+/g, "")) / 100;
+
+        if (isNegative(parsedAmount)) {
+          parsedEntry.Outflow = Math.abs(parsedAmount).toFixed(2);
+        } else {
+          parsedEntry.Inflow = Math.abs(parsedAmount).toFixed(2);
+        }
+      }
+
+      /**
+       * Parse category
+       */
+      const entryCategory = entryObject["Kategori"];
+      if (entryCategory) {
+        parsedEntry.Category = getMappedCategory(entryCategory);
+      }
+
+      /**
+       * Parse ayee
+       */
+      const entryPayee = entryObject["Tekst"];
+      if (entryPayee) {
+        parsedEntry.Payee = getMappedPayee(entryPayee).replace(
+          /^\s+|\s+$|\s+(?=\s)/g,
+          ""
+        );
+      }
+
+      output.push(parsedEntry);
+    });
+
+    return output;
+  }
+
+  const converter = csv({
+    delimiter: ";",
+    trim: true,
+    noheader: false,
+  });
+
+  const jsonObject = await converter.fromFile(fullPath);
+  const parsedObject = parseCSV(jsonObject);
+  console.log(">>>>>> parsedObject: ", parsedObject);
 }
 
 async function mainLoop() {
   /**
+   * 1. Sanitise CSV's.
+   *
    * Grab CSV files from input folder. Rename with sanitised file-name. Move to "sanitized" folder.
    */
   const unsanitisedCSVFiles: string[] = await getCSVFilePaths({
     basePath: INPUT_CSV_FILEPATH,
   });
 
-  const sanitizeJobs = unsanitisedCSVFiles.map((filePath, index) =>
-    sanitizeCSVFile({ filePath, index })
+  const sanitizeJobs = unsanitisedCSVFiles.map((filePath) =>
+    sanitizeCSVFile(filePath)
   );
 
   await Promise.all(sanitizeJobs);
 
   /**
+   * 2. Parse CSV's.
+   *
    * Parse all CSV files in sanitized folder.
    */
   const sanitisedCSVFiles: string[] = await getCSVFilePaths({
     basePath: SANITIZED_CSV_FILEPATH,
   });
 
-  const parseJobs = sanitisedCSVFiles.map((filePath, index) =>
-    parseCSVFile(filePath, index)
-  );
+  const parseJobs = sanitisedCSVFiles.map((filePath) => parseCSVFile(filePath));
 
   await Promise.all(parseJobs);
 }
@@ -90,3 +185,21 @@ async function mainLoop() {
     throw error;
   }
 })();
+
+function isNegative(input: number) {
+  return input < 0;
+}
+
+/**
+ * Map Danish input category to English name
+ */
+function getMappedCategory(inputCategory: string) {
+  return inputCategory;
+}
+
+/**
+ * Map Danish payee to readable name
+ */
+function getMappedPayee(inputPayee: string) {
+  return inputPayee;
+}
