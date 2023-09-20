09746f6581ff19f45fd4048cf02ecf901ae81323
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2023-09-21 00:18:09 +0200

    Update title of app

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Sydbank YNAB Parser',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Sydbank YNAB Parser',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
