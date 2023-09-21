b5923d0262a0621a96bd8d67bc96d720664ef495
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2023-09-21 20:24:35 +0200

    feat: Add file selection

--- src/renderer/DropArea.tsx
+++ src/renderer/DropArea.tsx
@@ -1,12 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import {
   Box,
   Text,
   Button,
   Stack,
-  ButtonGroup,
   Heading,
+  Input,
+  Flex,
+  IconButton,
+  InputGroup,
+  InputRightElement,
 } from '@chakra-ui/react';
+import { FaFileUpload } from 'react-icons/fa';
 
 interface DropAreaProps {}
 
@@ -14,6 +19,7 @@ const DropArea: React.FC<DropAreaProps> = ({}) => {
   const [droppedFilePath, setDroppedFilePath] = useState<string | null>(null);
   const [parsedData, setParsedData] = useState<string | null>(null);
   const [parsedFileName, setParsedFileName] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     const handleParseCsvResponse = (
@@ -82,6 +88,36 @@ const DropArea: React.FC<DropAreaProps> = ({}) => {
     setParsedFileName(null);
   };
 
+  const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      handleFileSelection(files[0]);
+    }
+  };
+
+  const handleFileSelection = (file: File | null) => {
+    if (file === null) {
+      setDroppedFilePath(null);
+      setParsedData(null);
+      setParsedFileName(null);
+    } else {
+      const filePath = file.path;
+
+      // Check if the selected file is a CSV file
+      if (filePath.toLowerCase().endsWith('.csv')) {
+        // Trigger an IPC message with the file path
+        setDroppedFilePath(filePath);
+        window.electron.ipcRenderer.send('parse-csv', filePath);
+      }
+    }
+  };
+
+  const handleOnClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <Box
       onDrop={handleDrop}
@@ -94,35 +130,64 @@ const DropArea: React.FC<DropAreaProps> = ({}) => {
       display="flex"
       justifyContent="center"
       alignItems="center"
-      cursor="pointer"
     >
-      <Box
-        p={6}
-        border="2px dashed #ccc"
-        borderRadius="md"
-        textAlign="center"
-        maxWidth={400}
-      >
-        <Stack spacing={4}>
-          <Heading>Sydbank YNAB Parser</Heading>
+      <Stack spacing={6}>
+        <Box
+          p={6}
+          border="2px dashed #ccc"
+          borderRadius="md"
+          textAlign="center"
+          background="whiteAlpha.50"
+          onClick={handleOnClick}
+          cursor="pointer"
+        >
+          <Stack spacing={4}>
+            <Heading>Sydbank YNAB Parser</Heading>
 
-          {!parsedData && <Text>Drag &amp; Drop a Sydbank CSV file here</Text>}
+            {!parsedData && (
+              <Stack spacing={2}>
+                <Text textAlign="center">
+                  Drag &amp; drop a CSV file into this area
+                </Text>
+                <Flex alignItems="center" justifyContent="center">
+                  <Text>
+                    Or click <b>here</b> to select one
+                  </Text>
+                  <Box ml={2}>
+                    <span>
+                      <FaFileUpload />
+                    </span>
+                  </Box>
+                </Flex>
+              </Stack>
+            )}
 
-          {parsedData && (
-            <Stack spacing={4}>
-              <Text mb={4}>
-                File path of the dropped CSV: {droppedFilePath}
-              </Text>
-              <Button colorScheme="telegram" onClick={downloadFormattedCSV}>
-                Download Formatted CSV
-              </Button>
-              <Button colorScheme="whiteAlpha" onClick={handleReset}>
-                Clear
-              </Button>
-            </Stack>
-          )}
-        </Stack>
-      </Box>
+            {parsedData && (
+              <Stack spacing={4}>
+                <Text mb={4}>
+                  File path of the dropped CSV: {droppedFilePath}
+                </Text>
+                <Button colorScheme="telegram" onClick={downloadFormattedCSV}>
+                  Download Formatted CSV
+                </Button>
+              </Stack>
+            )}
+          </Stack>
+        </Box>
+
+        {parsedData ? (
+          <Button colorScheme="whiteAlpha" onClick={handleReset}>
+            Clear
+          </Button>
+        ) : (
+          <Input
+            type="file"
+            ref={fileInputRef}
+            display="none"
+            onChange={handleFileInputChange}
+          />
+        )}
+      </Stack>
     </Box>
   );
 };
